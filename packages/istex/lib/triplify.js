3827c24a1b5b59271b36534d79299a2a5184a61b
François Parmentier <francois.parmentier@gmail.com>
2017-11-06 18:14:44 +0100

    bug: Make notices-to-doi work
    authors where translated as ISTEX/author/0/name, which needed a triplify.js adaptation.
    
    Warning: the transforms parameter is no more used.

--- packages/istex/lib/triplify.js
+++ packages/istex/lib/triplify.js
@@ -1,12 +1,3 @@
-// function transform(str, transformer) {
-//   if (!transformer || !Array.isArray(transformer) || transformer.length !== 2) {
-//     return `"${str}"`;
-//   }
-//   const pattern = transformer[0];
-//   const replacer = transformer[1];
-//   return str.replace(pattern, replacer);
-// }
-
 /**
  * Triplify data
  *
@@ -14,26 +5,22 @@
  * .pipe(ezs('ISTEXTriplify', {
  *    properties: {
  *      'ISTEX/doi/': 'http://purl.org/ontology/bibo/doi',
- *      'ISTEX/language/': 'http://purl.org/dc/terms/language'
+ *      'ISTEX/language/': 'http://purl.org/dc/terms/language',
+ *      'ISTEX/author/\\d/name': 'http://purl.org/dc/terms/creator',
+ *      'ISTEX/author/\\d/affiliations': 'https://data.istex.fr/ontology/istex#affiliation',
  *    },
- *    transforms: {
- *      'ISTEX/language/': [
- *        /([a-z]{3})/,
- *        '<http://lexvo.org/id/iso639-3/$1>'
- *      ]
- *    }
  *  ));
  * --->
  * <https://data.istex.fr/document/2FF3F5B1477986B9C617BB75CA3333DBEE99EB05>
  *   a <http://purl.org/ontology/bibo/Document> ;
- *   <http://purl.org/dc/terms/language> <http://lexvo.org/id/iso639-3/fra> ;
  *   <http://purl.org/ontology/bibo/doi>  "10.1002/zaac.19936190205" ;
  *   <https://data.istex.fr/ontology/istex#idIstex> "2FF3F5B1477986B9C617BB75CA3333DBEE99EB05" .
- *
+ *   <http://purl.org/dc/terms/creator> "Steve Parker" .
+ *   <https://data.istex.fr/ontology/istex#affiliation> "Graduate Institute of Applied Linguistics" .
+ *   <https://data.istex.fr/ontology/istex#affiliation> "E-mail: steve-monica_parker@sil.org" .
  */
 module.exports = function triplify(data, feed) {
   const properties = this.getParam('properties', {});
-  const transforms = this.getParam('transforms', null);
 
   if (this.isLast()) {
     feed.close();
@@ -42,43 +29,32 @@ module.exports = function triplify(data, feed) {
 
   feed.write(`<https://data.istex.fr/document/${data['ISTEX/id']}> <https://data.istex.fr/ontology/istex#idIstex> "${data['ISTEX/id']}" .\n`);
   feed.write(`<https://data.istex.fr/document/${data['ISTEX/id']}> a <http://purl.org/ontology/bibo/Document> .\n`);
+
+  const dataArray = Object.entries(data);
+
   Object.keys(properties).forEach((path) => {
-    if (path[path.length - 1] === '/') {
-      // path points to an array
-      const values = [];
-      let i = 0;
-      while (data[path + i]) {
-        values.push(data[path + i]);
-        i += 1;
-      }
-      if (values.length) {
-        for (i = 0; i < values.length; i += 1) {
-          let value = values[i];
-          if (transforms && transforms[path]) {
-            value = value.replace(transforms[path][0], transforms[path][1]);
-          } else {
-            value = `"${value}"`;
-          }
-          feed.write(`<https://data.istex.fr/document/${data['ISTEX/id']}> <${properties[path]}> ${value} .\n`);
-        }
-      }
-    } else {
-      // Path points to a single value
-      let value = data[path];
-      if (transforms && transforms[path]) {
-        value = value.replace(transforms[path][0], transforms[path][1]);
-      } else {
-        value = `"${value}"`;
-      }
-      feed.write(`<https://data.istex.fr/document/${data['ISTEX/id']}> <${properties[path]}> ${value} .\n`);
-    }
+    dataArray.filter(([
+      key,
+    ]) => key.match(new RegExp(path)))
+    .forEach(([
+      ,
+      value,
+    ]) => {
+      feed.write(`<https://data.istex.fr/document/${data['ISTEX/id']}> <${properties[path]}> "${value}" .\n`);
+    });
   });
 
   feed.end();
 };
 
-// <https://data.istex.fr/document/2FF3F5B1477986B9C617BB75CA3333DBEE99EB05>
-//               a bibo:Document ;
-//               dcterms:language <http://lexvo.org/id/iso639-3/fra> ;
-//               bibo:doi  "10.1002/zaac.19936190205" ;
-//               istex:idIstex "2FF3F5B1477986B9C617BB75CA3333DBEE99EB05" .
+// data: {
+// ...
+// 'ISTEX/author/0/name': 'Geoffrey Strickland',
+// 'ISTEX/author/0/affiliations/0': 'University of Reading',
+// 'ISTEX/host/issn/0': '0047-2441',
+// 'ISTEX/host/eissn/0': '1740-2379',
+// 'ISTEX/title': 'Maupassant, Zola, Jules Vallès and the Paris Commune of 1871',
+// 'ISTEX/publicationDate': '1983',
+// 'ISTEX/doi/0': '10.1177/004724418301305203',
+// 'ISTEX/id': 'F6CB7249E90BD96D5F7E3C4E80CC1C3FEE4FF483',
+// 'ISTEX/score': 1 }
