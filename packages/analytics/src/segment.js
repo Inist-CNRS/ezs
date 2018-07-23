be2fccf6c6ada12cd74bad077bf829511f5f6567
Nicolas Thouvenin <nthouvenin@gmail.com>
2018-07-23 16:29:49 +0200

    improve "segment" statement

--- packages/analytics/src/segment.js
+++ packages/analytics/src/segment.js
@@ -1,10 +1,12 @@
 import get from 'lodash.get';
+import flatten from 'lodash.flatten';
 import core from './core';
 /**
- * Take `Object` object getting some fields with json path, and 
+ * Take `Object` object getting some fields with json path, and
  * throw segment of value. Ex: get [a,b,c] and throw [a,b], [b,c]
  *
  * @param {String} [path=value] path
+ * @param {Boolean} [aggregate=true] aggregate all values for all paths (or not)
  * @returns {Object}
  */
 export default function segment(data, feed) {
@@ -12,20 +14,34 @@ export default function segment(data, feed) {
         feed.close();
         return;
     }
+    const aggr = this.getParam('aggregate', true);
     const path = this.getParam('path', 'value');
     const fields = Array.isArray(path) ? path : [path];
 
-    fields
+    const valuesOrig = fields
         .filter(k => typeof k === 'string')
         .map(key => get(data, key))
         .filter(x => x)
-        .map(item => (item instanceof Array ? item : [item]))
-        .reduce((pre, cur) => pre.concat(cur), [])
+        .map(item => (Array.isArray(item) ? item : [item]))
+
+    const values = Array.isArray(valuesOrig[0][0]) ? flatten(valuesOrig) : valuesOrig;
+
+    if (aggr) {
+        values.reduce((pre, cur) => pre.concat(cur), [])
         .reduce((pre, cur) => {
             if (pre) {
                 feed.write(core([pre, cur], 1))
             }
             return cur;
-        });
+        }, false);
+    } else {
+        values.map(item => item.reduce((pre, cur) => {
+            if (pre) {
+                feed.write(core([pre, cur], 1))
+            }
+            return cur;
+        }, false));
+    }
+
     feed.end();
 }
--- packages/analytics/test/network.js
+++ packages/analytics/test/network.js
@@ -28,7 +28,7 @@ describe('network', () => {
             });
     });
 
-    it('segment', (done) => {
+    it('segment #1', (done) => {
         const res = [];
         from([
             {
@@ -81,6 +81,50 @@ describe('network', () => {
                 done();
             });
     });
+
+    it('segment #2', (done) => {
+        const res = [];
+        from([
+            {
+                id: 'doc#1',
+                a: [ 1, 2, 3 ],
+                b: [ 1, 2, 4 ]
+            },
+            {
+                id: 'doc#2',
+                a: [ 1, 2, 4 ],
+                b: [ 1, 2, 4 ]
+            },
+            {
+                id: 'doc#3',
+                c: [
+                    [ 1, 2, 6 ],
+                    [ 1, 2, 7 ]
+                ]
+            },
+            {
+                id: 'doc#4',
+                a: [ 1, 2, 3 ],
+                b: [ 1, 2, 7 ]
+            },
+        ])
+            .pipe(ezs('segment', { aggregate : false, path: ['a', 'b', 'c'] }))
+            .on('data', (chunk) => {
+                assert(typeof chunk === 'object');
+                res.push(chunk);
+            })
+            .on('end', () => {
+                assert.equal(1, res[0].id[0]);
+                assert.equal(2, res[0].id[1]);
+                assert.equal(2, res[1].id[0]);
+                assert.equal(3, res[1].id[1]);
+                assert.equal(1, res[2].id[0]);
+                assert.equal(2, res[2].id[1]);
+                assert.equal(16, res.length);
+                done();
+            });
+    });
+
     it('pair', (done) => {
         const res = [];
         from([
