a7a9d1615d9683ffe3d607a8e65576d3fa361bd4
Nicolas Thouvenin <nthouvenin@gmail.com>
2019-02-05 10:41:45 +0100

    no download instruction

--- packages/core/src/server/serverPipeline.js
+++ packages/core/src/server/serverPipeline.js
@@ -5,7 +5,6 @@ import { isFile } from '../file';
 const serverPipeline = ezs => (request, response) => {
     const { url } = request;
     const filePath = ezs.fileToServe(url);
-    const { fileName } = path.parse(filePath);
     if (!isFile(filePath)) {
         response.writeHead(404);
         response.end();
@@ -13,7 +12,6 @@ const serverPipeline = ezs => (request, response) => {
     }
     response.writeHead(200, {
         'Content-Type': 'text/plain',
-        'Content-Disposition': `attachment; filename=${fileName}`,
     });
     createReadStream(filePath).pipe(response);
     return true;
