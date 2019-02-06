aa65da765de145238ac018e99da0b15124ba801e
Nicolas Thouvenin <nthouvenin@gmail.com>
2019-02-06 14:46:29 +0100

    Merge pull request #16 from touv/imporve-server-informations
    list scripts

--- packages/core/src/server/serverInformation.js
+++ packages/core/src/server/serverInformation.js
@@ -1,24 +1,46 @@
+import dir from 'node-dir';
+import Parameter from '../parameter';
 import {
-    PORT, VERSION, NCPUS, STARTED_AT,
+    DEBUG, PORT, VERSION, NCPUS, STARTED_AT,
 } from '../constants';
 
-const infoSince = startedAt => ({
-    concurrency: NCPUS,
-    uptime: Date.now() - startedAt,
-    timestamp: Date.now(),
-    version: VERSION,
-    port: PORT,
+const getInformations = dirPath => new Promise((resolve, reject) => {
+    dir.files(dirPath, (err, files) => {
+        if (err) {
+            return reject(err);
+        }
+        const scripts = files
+            .filter(f => (f.search(/\.(ini|ezs)$/) > 0))
+            .map(f => f.replace(dirPath, `http://127.0.0.1:${PORT}`));
+        return resolve({
+            concurrency: NCPUS,
+            uptime: Date.now() - STARTED_AT,
+            timestamp: Date.now(),
+            version: VERSION,
+            scripts,
+        });
+    });
 });
 
-const serverInformation = () => (request, response) => {
-    const responseBody = JSON.stringify(infoSince(STARTED_AT));
-    const responseHeaders = {
-        'Content-Type': 'application/json',
-        'Content-Length': Buffer.byteLength(responseBody),
-    };
-    response.writeHead(200, responseHeaders);
-    response.write(responseBody);
-    response.end();
+const serverInformation = ezs => (request, response) => {
+    getInformations(ezs.fileToServe(''))
+        .then((informations) => {
+            const responseBody = JSON.stringify(informations);
+            const responseHeaders = {
+                'Content-Type': 'application/json',
+                'Content-Length': Buffer.byteLength(responseBody),
+            };
+            response.writeHead(200, responseHeaders);
+            response.write(responseBody);
+            response.end();
+        })
+        .catch((error) => {
+            DEBUG('Server has caught an error', error);
+            if (!response.headersSent) {
+                response.writeHead(500, { 'X-Error': Parameter.encode(error.toString()) });
+            }
+            response.end();
+        });
 };
 
 export default serverInformation;
