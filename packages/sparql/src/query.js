78859a9a2f84664ec065e9c09f39aac4a906856c
Antoine Pinot <antoine.pinot1@gmail.com>
2019-05-23 15:02:43 +0200

    complete code for SPARQLQuery instruction

--- packages/sparql/src/query.js
+++ packages/sparql/src/query.js
@@ -1,7 +1,38 @@
-export default function SPARQLQuery(data, feed) {
+const fetch = require('fetch-with-proxy').default;
+
+export default async function SPARQLQuery(data, feed) {
     if (this.isLast()) {
         return feed.close();
     }
-    feed.write(data);
-    feed.end();
-} 
+
+    const { query, endpoint } = data;
+    if (!query) {
+        throw new Error('No query given!');
+    }
+    if (!endpoint) {
+        throw new Error('No sparql endpoint given !');
+    }
+
+    const encoded = encodeURIComponent(query);
+    const requestUrl = `${endpoint}?query=${encoded}`;
+
+    const options = {
+        headers: {
+            Accept: 'application/sparql-results+json',
+        },
+    };
+
+    const response = await fetch(requestUrl, options);
+    if (!response.ok) {
+        throw new Error('Can not connect to the sparql endpoint !');
+    }
+    let responseData;
+    try {
+        responseData = await response.json();
+    } catch (error) {
+        throw new Error('The data is not JSON object !');
+    }
+
+    feed.write(responseData);
+    return feed.end();
+}
