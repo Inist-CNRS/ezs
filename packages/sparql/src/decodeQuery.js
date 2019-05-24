8075ad57a95ad88763daad8e8422b8576337c56d
Antoine Pinot <antoine.pinot1@gmail.com>
2019-05-24 09:36:59 +0200

    rename link var to linkQuery

--- packages/sparql/src/decodeQuery.js
+++ packages/sparql/src/decodeQuery.js
@@ -2,11 +2,11 @@ export default function SPARQLDecodeQuery(data, feed) {
     if (this.isLast()) {
         return feed.close();
     }
-    const { link } = data;
-    if (!link) {
+    const { linkQuery } = data;
+    if (!linkQuery) {
         throw new Error('No share link given !');
     }
-    const reduced = link.substr(link.indexOf('#') + 1);
+    const reduced = linkQuery.substr(linkQuery.indexOf('#') + 1);
     const keysValues = reduced.split('&').map(elem => elem.split('='));
     const cleaned = keysValues.map(keyValue => keyValue.map(elem => decodeURIComponent(elem.replace(/\+/g, ' '))));
     const result = cleaned.reduce((acc, cur) => {
--- packages/sparql/test/decodeQuery.spec.js
+++ packages/sparql/test/decodeQuery.spec.js
@@ -7,7 +7,7 @@ ezs.use(statements);
 
 test('get an error when empty link', (done) => {
     // TODO: add query
-    from([{ link: '' }])
+    from([{ linkQuery: '' }])
         .pipe(ezs('SPARQLDecodeQuery'))
         .pipe(ezs.catch(e => e))
         .on('data', () => {
@@ -20,7 +20,7 @@ test('get an error when empty link', (done) => {
 });
 
 test('get an error when incorrect link', (done) => {
-    from([{ link: 'http://192.168.31.146:49452/triplestore/sparql/#linkincorrect' }])
+    from([{ linkQuery: 'http://192.168.31.146:49452/triplestore/sparql/#linkincorrect' }])
         .pipe(ezs('SPARQLDecodeQuery'))
         .pipe(ezs.catch(e => e))
         .on('data', () => {
@@ -33,7 +33,7 @@ test('get an error when incorrect link', (done) => {
 });
 
 test('should have the corresponding query and endpoint', (done) => {
-    from([{ link: 'http://192.168.31.146:49452/triplestore/sparql/#query=SELECT+*%0AWHERE+%7B%0A++%3Fsubject+%3Fverb+%3Fcomplement+.%0A%7D%0ALIMIT+100&contentTypeConstruct=text%2Fturtle&contentTypeSelect=application%2Fsparql-results%2Bjson&endpoint=https%3A%2F%2Fdata.istex.fr%2Fsparql%2F&requestMethod=POST&tabTitle=Query&headers=%7B%7D&outputFormat=table' }])
+    from([{ linkQuery: 'http://192.168.31.146:49452/triplestore/sparql/#query=SELECT+*%0AWHERE+%7B%0A++%3Fsubject+%3Fverb+%3Fcomplement+.%0A%7D%0ALIMIT+100&contentTypeConstruct=text%2Fturtle&contentTypeSelect=application%2Fsparql-results%2Bjson&endpoint=https%3A%2F%2Fdata.istex.fr%2Fsparql%2F&requestMethod=POST&tabTitle=Query&headers=%7B%7D&outputFormat=table' }])
         .pipe(ezs('SPARQLDecodeQuery'))
         .on('data', (data) => {
             const { query, endpoint } = data;
