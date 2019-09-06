0ffb2522ec0629f87f4d14922be9f40ad554592b
François Parmentier <francois.parmentier@gmail.com>
2019-09-06 09:29:16 +0200

    fix(basics): Fix the utils source file

--- packages/basics/src/utils.js
+++ packages/basics/src/utils.js
@@ -1,9 +1,9 @@
-export function writeTo(stream, data, cb) {
+export const writeTo = function writeTo(stream, data, cb) {
     if (!stream.write(data)) {
         stream.once('drain', cb);
     } else {
         process.nextTick(cb);
     }
-}
+};
 
 export default writeTo;
