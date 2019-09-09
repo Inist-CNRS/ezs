4d4314432c6493dd8426a7f8e5710667a61577eb
François Parmentier <francois.parmentier@gmail.com>
2019-09-09 09:25:08 +0200

    test(basics): Add OBJCount test

---
+++ packages/basics/test/count.js
@@ -0,0 +1,27 @@
+import from from 'from';
+import ezs from '../../core/src';
+import ezsBasics from '../src';
+
+ezs.use(ezsBasics);
+
+describe('OBJCount', () => {
+    it('should return a number', (done) => {
+        from(['a'])
+            .pipe(ezs('OBJCount'))
+            .on('data', (data) => {
+                expect(data).toBe(2);
+                expect(typeof data).toBe('number');
+                done();
+            });
+    });
+
+    it('should return the number of chunks', (done) => {
+        from(['a', 'b', 'c', 'd'])
+            .pipe(ezs('OBJCount'))
+            .on('data', (data) => {
+                // TODO: check it is really the expected behaviour
+                expect(data).toBe(5);
+                done();
+            });
+    });
+});
