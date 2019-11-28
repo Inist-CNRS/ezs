15c325248173a9ce53e97d144017dda0d3fdcf3d
François Parmentier <francois.parmentier@gmail.com>
2019-11-28 11:45:02 +0100

    chore(conditor): Update RNSR.json with new depleteString

--- packages/conditor/src/strings.js
+++ packages/conditor/src/strings.js
@@ -1,11 +1,13 @@
 import unidecode from 'unidecode';
-import { compose, toLower } from 'ramda';
+import { compose, replace, toLower } from 'ramda';
 
 export const removeAccents = (str) => unidecode(str);
+export const removeDash = replace('-', ' ');
+export const removeQuote = replace("'", ' ');
 /**
  * Deplete string from accents and upper case.
  * @param {string} str
  * @returns {string}
  * @private
  */
-export const depleteString = compose(toLower, removeAccents, String);
+export const depleteString = compose(toLower, removeAccents, removeQuote, removeDash, String);
