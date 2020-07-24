0b98c0a62096b217925866c1062ed54f6c3074cf
bouri-mounir <mounir.bouri@esprit.tn>
2020-07-24 17:05:04 +0200

    use pipe instead of async/await functions

--- packages/loterre/src/skos-pathenum.spec.js
+++ packages/loterre/src/skos-pathenum.spec.js
@@ -15,22 +15,6 @@ describe('SKOSPathEnum', () => {
     beforeEach(() => {
         jest.setTimeout(500000);
     });
-    test('from large file : CHEBI.skos', (done) => {
-        const output = [];
-        const input = path.resolve('./packages/loterre/src/data/CHEBI.skos');
-        createReadStream(input)
-            .pipe(ezs('concat'))
-            .pipe(ezs('XMLParse', { separator: ' /rdf:RDF/skos:Concept' }))
-            .pipe(ezs('SKOSObject'))
-            .pipe(ezs('SKOSPathEnum', { language: 'en' }))
-            .pipe(ezs.catch())
-            .on('error', done)
-            .on('data', (chunk) => output.push(chunk))
-            .on('end', () => {
-                expect(output.length).toBe(125748);
-                done();
-            });
-    });
 
     test('from file : data-sample.skos', (done) => {
         const output = [];
@@ -45,8 +29,8 @@ describe('SKOSPathEnum', () => {
             .on('data', (chunk) => output.push(chunk))
             .on('end', () => {
                 expect(output.length).toBe(3);
+                expect(output[0].broader).toEqual(expect.arrayContaining([{ key: 'http://example.com/dishes#potatoBased', label: 'Plats à base de pomme de terre' }]));
                 expect(output[1].broader).toEqual(expect.arrayContaining([{ key: 'http://example.com/dishes#potatoBased', label: 'Plats à base de pomme de terre' }]));
-                expect(output[2].broader).toEqual(expect.arrayContaining([{ key: 'http://example.com/dishes#potatoBased', label: 'Plats à base de pomme de terre' }]));
                 done();
             });
     });
@@ -64,40 +48,50 @@ describe('SKOSPathEnum', () => {
             .on('data', (chunk) => output.push(chunk))
             .on('end', () => {
                 expect(output.length).toBe(171);
-                expect(output[100]['prefLabel@fr']).toEqual('platinoïde');
-                expect(output[100].broader).toEqual(expect.arrayContaining([
+                expect(output[10]['prefLabel@fr']).toEqual('cobalt');
+                expect(output[10].broader).toEqual(expect.arrayContaining([
                     {
                         key: 'http://data.loterre.fr/ark:/67375/8HQ-NM5G007X-D',
                         label: 'métal de transition'
                     }
                 ]));
-                expect(output[100].narrower).toEqual(expect.arrayContaining([
+                expect(output[10].broader).toEqual(expect.arrayContaining([
                     {
-                        key: 'http://data.loterre.fr/ark:/67375/8HQ-R6LMKQKX-5',
-                        label: 'ruthénium'
+                        key: 'http://data.loterre.fr/ark:/67375/8HQ-NM5G007X-D',
+                        label: 'métal de transition'
                     },
                     {
-                        key: 'http://data.loterre.fr/ark:/67375/8HQ-FKKNQWB0-B',
-                        label: 'rhodium'
+                        key: 'http://data.loterre.fr/ark:/67375/8HQ-ZR503FCG-D',
+                        label: 'élément du bloc d'
                     },
                     {
-                        key: 'http://data.loterre.fr/ark:/67375/8HQ-GKH9PL2W-2',
-                        label: 'palladium'
+                        key: 'http://data.loterre.fr/ark:/67375/8HQ-WGZD8DMF-Z',
+                        label: 'élément de la période 4'
                     },
                     {
-                        key: 'http://data.loterre.fr/ark:/67375/8HQ-V9RXLN1J-0',
-                        label: 'osmium'
-                    },
-                    {
-                        key: 'http://data.loterre.fr/ark:/67375/8HQ-WK9PDRSF-D',
-                        label: 'iridium'
-                    },
-                    {
-                        key: 'http://data.loterre.fr/ark:/67375/8HQ-HCSZZCGN-4',
-                        label: 'platine'
+                        key: 'http://data.loterre.fr/ark:/67375/8HQ-VVBTC56K-P',
+                        label: 'élément du groupe 9'
                     }
                 ]));
                 done();
             });
     });
+    /*
+    test('from file : CHEBI.skos', (done) => {
+        const output = [];
+        const input = path.resolve('./packages/loterre/src/data/CHEBI.skos');
+        createReadStream(input)
+            .pipe(ezs('concat'))
+            .pipe(ezs('XMLParse', { separator: ' /rdf:RDF/skos:Concept' }))
+            .pipe(ezs('SKOSObject'))
+            .pipe(ezs('SKOSPathEnum', { language: 'fr' }))
+            .pipe(ezs.catch())
+            .on('error', done)
+            .on('data', (chunk) => output.push(chunk))
+            .on('end', () => {
+                expect(output.length).toBe(125748);
+                done();
+            });
+    });
+    */
 });
