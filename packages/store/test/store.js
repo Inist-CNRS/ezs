bc4f20f75e84447a0081fa32701525a27392e3f2
Nicolas Thouvenin <nthouvenin@gmail.com>
2020-07-27 21:51:50 +0200

    test: 💍 add some tests

--- packages/store/test/store.js
+++ packages/store/test/store.js
@@ -1,9 +1,10 @@
 import ezs from '../../core/src';
-import { createStore }  from '../src/store';
+import { createStore, createStoreWithID } from '../src/store';
 
 describe('Store', () => {
-    it('add distinct values', async (done) => {
+    it('add distinct values #1', async (done) => {
         const store = createStore(ezs, 'test_store1');
+        expect(store.id()).toEqual(expect.stringContaining('test_store1'));
         await Promise.all([
             store.add(1, 'A'),
             store.add(2, 'B'),
@@ -23,6 +24,70 @@ describe('Store', () => {
                 done();
             });
     });
+
+    it('add distinct values #2', async (done) => {
+        const store = createStore(ezs, 'test_store2');
+        expect(store.id()).toEqual(expect.stringContaining('test_store2'));
+        await Promise.all([
+            store.add(1, 'A'),
+            store.add(2, 'B'),
+            store.add(3, 'C'),
+        ]);
+        const output = [];
+        store
+            .stream()
+            .on('data', (chunk) => {
+                output.push(chunk);
+            })
+            .on('end', () => {
+                expect(output.length).toEqual(3);
+                expect(output[0].id).toEqual(1);
+                expect(output[0].value[0]).toEqual('A');
+                store.close();
+                done();
+            });
+    });
+
+
+    it('add distinct values #3', async (done) => {
+        const store = createStore(ezs, 'test_store3');
+        await Promise.all([
+            store.add(1, 'A'),
+            store.add(2, 'B'),
+            store.add(3, 'C'),
+        ]);
+        const k1 = await store.get(1);
+        expect(k1.shift()).toEqual('A');
+        const k2 = await store.get(2);
+        expect(k2.shift()).toEqual('B');
+        const k3 = await store.get(3);
+        expect(k3.shift()).toEqual('C');
+        done();
+    });
+
+    it('add distinct values #4', async (done) => {
+        const store = createStoreWithID(ezs, 'test_store4');
+        expect(store.id()).toEqual('test_store4');
+        await Promise.all([
+            store.add(1, 'A'),
+            store.add(2, 'B'),
+            store.add(3, 'C'),
+        ]);
+        const output = [];
+        store
+            .empty()
+            .on('data', (chunk) => {
+                output.push(chunk);
+            })
+            .on('end', () => {
+                expect(output.length).toEqual(3);
+                expect(output[0].id).toEqual(1);
+                expect(output[0].value[0]).toEqual('A');
+                store.close();
+                done();
+            });
+    });
+
 
     it('add duplicate keys', async (done) => {
         const store = createStore(ezs, 'test_store2');
@@ -80,4 +145,23 @@ describe('Store', () => {
             });
     });
 
+    it('add no value #1', async (done) => {
+        const store = createStore(ezs, 'test_store6');
+        expect(store.id()).toEqual(expect.stringContaining('test_store6'));
+        await Promise.all([
+            store.add(1, 'A'),
+            store.add(2, 'B'),
+            store.add(3, 'C'),
+        ]);
+        const k1 = await store.get(1);
+        expect(k1.shift()).toEqual('A');
+        const k2 = await store.get(2);
+        expect(k2.shift()).toEqual('B');
+        const k3 = await store.get(3);
+        expect(k3.shift()).toEqual('C');
+        const k4 = await store.get(4);
+        expect(k4).toBeNull();
+        await store.reset();
+        done();
+    });
 });
