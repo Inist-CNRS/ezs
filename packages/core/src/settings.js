3d935d0d22fec73098457c16f5261b950d4c5732
Nicolas Thouvenin <nthouvenin@gmail.com>
2020-09-16 23:04:26 +0200

    feat: 🎸 set cache delay with env var

--- packages/core/src/settings.js
+++ packages/core/src/settings.js
@@ -7,6 +7,7 @@ const encoding = String(process.env.EZS_ENCODING || 'gzip');
 const port = Number(process.env.EZS_PORT || 31976);
 const cacheEnable = Boolean(autocast(process.env.EZS_CACHE));
 const nShards = Number(process.env.EZS_NSHARDS || 16);
+const cacheDelay = Number(process.env.EZS_DELAY || 3600);
 const settings = {
     highWaterMark: {
         object: nShards,
@@ -20,8 +21,10 @@ const settings = {
     port,
     server: process.env.EZS_SERVER ? String(process.env.EZS_SERVER).split(',').map((h) => h.trim()) : null,
     cacheEnable,
+    cacheDelay,
     cache: {
         max: (nShards * 256),
+        maxAge: (cacheDelay * 1000),
     },
     queue: {
         concurrency: nShards,
