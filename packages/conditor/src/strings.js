d28685a2454b16550a2e22d04e8c4d01ea904afc
François Parmentier <francois.parmentier@gmail.com>
2021-01-27 10:56:39 +0100

    style(conditor): Lint regex in strings

--- packages/conditor/src/strings.js
+++ packages/conditor/src/strings.js
@@ -2,7 +2,7 @@ import unidecode from 'unidecode';
 import { compose, replace, toLower } from 'ramda';
 
 export const removeAccents = (str) => unidecode(str);
-export const removeDash = replace(/\-/g, ' ');
+export const removeDash = replace(/-/g, ' ');
 export const removeQuote = replace(/'/g, ' ');
 /**
  * Deplete string from accents, upper case, dash and simple quotes.
