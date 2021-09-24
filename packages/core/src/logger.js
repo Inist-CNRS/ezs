05d110ab2523ebee9c62ffc6af8575340a89a9f1
Nicolas Thouvenin <nthouvenin@gmail.com>
2021-09-24 17:18:46 +0200

    fix: 🐛 enable metrics for cli

--- packages/core/src/logger.js
+++ packages/core/src/logger.js
@@ -1,7 +1,6 @@
 import _ from 'lodash';
 import settings from './settings';
 
-const { metricsEnable } = settings;
 export const getMetricLogger = (stage, bucket) => {
     const stages = []
         .concat(stage)
@@ -17,7 +16,7 @@ export const getMetricLogger = (stage, bucket) => {
         .join('');
     const timestamp = Date.now();
     const labels = `${stages},${buckets}`;
-    if (metricsEnable) {
+    if (settings.metricsEnable) {
         return (name, value) => process.stderr.write(`${name}{${labels}} ${value} ${timestamp}\n`);
     }
     return (name, value) => true;
