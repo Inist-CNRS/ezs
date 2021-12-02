9e27f2ad8d968757b563e057d3b732169f91b6db
François Parmentier <francois.parmentier@gmail.com>
2021-12-02 15:27:31 +0100

    fix(teeft): Fix latest test
    When a path to toLowerCase is a single-element array.

--- packages/teeft/src/to-lower-case.js
+++ packages/teeft/src/to-lower-case.js
@@ -17,20 +17,19 @@ const objPropLower = curry((propPath, obj) => {
  * Transform strings to lower case.
  *
  * @export
- * @param {any} data
- * @param {any} feed
  * @param {Array<string>}   [path=[]]    path to the property to modify
  */
-export default function ToLowerCase(data, feed) {
+export default function TeeftToLowerCase(data, feed) {
     if (this.isLast()) {
         return feed.close();
     }
     const paramPath = this.getParam('path', []);
+    const arrayPath = Array.isArray(paramPath) ? paramPath : [paramPath];
     let res;
     if (Array.isArray(data)) {
-        res = map(objPropLower(paramPath), data);
+        res = map(objPropLower(arrayPath), data);
     } else {
-        res = objPropLower(paramPath, data);
+        res = objPropLower(arrayPath, data);
     }
     feed.write(res);
     feed.end();
