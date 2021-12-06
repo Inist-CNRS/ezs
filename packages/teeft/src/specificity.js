5ef9d4d6c1c772be1625a52cbba64d8fd0309252
François Parmentier <francois.parmentier@gmail.com>
2021-12-06 14:03:37 +0100

    refactor(teeft): Move functions and counters into the statement
    To prevent parallel executions to mess up with the counters.

--- packages/teeft/src/specificity.js
+++ packages/teeft/src/specificity.js
@@ -6,54 +6,10 @@ import { getResource } from './stop-words';
 // Cache for dictionnary weights
 const dictionaryWeights = {};
 
-// NOTE: for all these variables, it should be better to place them within the
-// scope of the main function.
-// The same for the following functions (addSpecificity, normalizeSpecificity,
-// addNormalizedSpecificity), to use its closure, but they would increase the
-// main function's size.
-let totalFrequencies;
 let weights = {};
-let maxSpecificity;
-let specificitySum;
-
-function initialize() {
-    totalFrequencies = 0;
-    maxSpecificity = 0;
-    specificitySum = 0;
-}
 
 const isMulti = term => !term.tag;
 const isMono = term => term.tag;
-
-function addSpecificity(oldTerm) {
-    const { frequency, term } = oldTerm;
-    const weight = weights[term] || 10 ** -5;
-    const computeSpecificity = pipe(
-        divide(__, totalFrequencies),
-        divide(__, weight),
-    );
-    const specificity = computeSpecificity(frequency);
-
-    maxSpecificity = max(maxSpecificity, specificity);
-    return {
-        ...oldTerm,
-        specificity,
-    };
-}
-
-function normalizeSpecificity(token) {
-    const specificity = token.specificity / maxSpecificity;
-    specificitySum += isMono(token) ? specificity : 0;
-    return {
-        ...token,
-        specificity,
-    };
-}
-
-const addNormalizedSpecificity = pipe(
-    map(addSpecificity),
-    map(normalizeSpecificity),
-);
 
 /**
  * Take documents (with a `path`, an array of `terms`, each term being an object
@@ -73,12 +29,52 @@ const addNormalizedSpecificity = pipe(
  * @returns
  */
 export default async function TeeftSpecificity(data, feed) {
-    const weightedDictionary = this.getParam('weightedDictionary', 'Ress_Frantext');
-    const filter = this.getParam('filter', true);
-    const sort = this.getParam('sort', false);
     if (this.isLast()) {
         return feed.close();
     }
+
+    // #region Functions
+    const self = this;
+    self.totalFrequencies = 0;
+    self.maxSpecificity = 0;
+    self.specificitySum = 0;
+
+    function addSpecificity(oldTerm) {
+        const { frequency, term } = oldTerm;
+        const weight = weights[term] || 10 ** -5;
+        const computeSpecificity = pipe(
+            divide(__, self.totalFrequencies),
+            divide(__, weight),
+        );
+        const specificity = computeSpecificity(frequency);
+
+        self.maxSpecificity = max(self.maxSpecificity, specificity);
+        return {
+            ...oldTerm,
+            specificity,
+        };
+    }
+
+    function normalizeSpecificity(term) {
+        const specificity = term.specificity / self.maxSpecificity;
+        self.specificitySum += isMono(term) ? specificity : 0;
+        return {
+            ...term,
+            specificity,
+        };
+    }
+
+    const addNormalizedSpecificity = pipe(
+        map(addSpecificity),
+        map(normalizeSpecificity),
+    );
+    // #endregion Functions
+
+    // Parameters
+    const weightedDictionary = this.getParam('weightedDictionary', 'Ress_Frantext');
+    const filter = this.getParam('filter', true);
+    const sort = this.getParam('sort', false);
+
     if (this.isFirst()) {
         weights = {};
         if (weightedDictionary) {
@@ -93,15 +89,14 @@ export default async function TeeftSpecificity(data, feed) {
         }
     }
     const docIn = data;
-    initialize();
 
-    totalFrequencies = sum(docIn.terms.map(prop('frequency')));
+    self.totalFrequencies = sum(docIn.terms.map(prop('frequency')));
 
     let terms = addNormalizedSpecificity(docIn.terms);
 
     if (filter) {
         // compute averageSpecificity only on monoTerms
-        const averageSpecificity = specificitySum / terms.filter(isMono).length;
+        const averageSpecificity = self.specificitySum / terms.filter(isMono).length;
         terms = terms.filter(term => term.specificity >= averageSpecificity || isMulti(term));
     }
 
