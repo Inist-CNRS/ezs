e7c3da541b06fa643330a8ea562f4d140a8e532f
Nicolas Thouvenin <nthouvenin@gmail.com>
2022-09-16 09:31:49 +0200

    fix: new name

--- packages/analytics/test/combine.js
+++ packages/analytics/test/combine.js
@@ -304,7 +304,7 @@ describe('no combine', () => {
             [use]
             plugin = analytics
 
-            [files]
+            [FILELoad]
             location = ${__dirname}
         `;
         const filename = `${__dirname}/forbidden`;
