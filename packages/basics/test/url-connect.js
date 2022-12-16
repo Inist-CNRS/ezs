c3d56f30ce20d56c94328c33fc5cf451177291be
Nicolas Thouvenin <nthouvenin@gmail.com>
2022-12-16 16:00:55 +0100

    test: 💍 add new tests

--- packages/basics/test/url-connect.js
+++ packages/basics/test/url-connect.js
@@ -1,3 +1,4 @@
+import http from 'http';
 import from from 'from';
 import semver from 'semver';
 import ezs from '../../core/src';
@@ -6,8 +7,8 @@ import statements from '../src';
 ezs.addPath(__dirname);
 ezs.settings.servePath = __dirname;
 
-let serverHandle;
 describe('URLConnect', () => {
+    let serverHandle;
     beforeAll((ready) => {
         serverHandle = ezs.createServer(33331, __dirname);
         serverHandle.on('listening', () => ready());
@@ -269,3 +270,171 @@ describe('URLConnect', () => {
             });
     }, 10000);
 });
+
+describe('URLConnect (BIS)', () => {
+    let serverHandle1;
+    let counter = 0;
+    beforeAll((ready) => {
+        serverHandle1 = http.createServer((req, res) => {
+            res.writeHead(200);
+            if (req.headers['x-timeout'] === 'all') {
+                const timeoutHandle = setTimeout(() => {
+                    req.pipe(res);
+                }, 1000);
+                res.on('close', () => clearTimeout(timeoutHandle));
+            }
+            if (req.headers['x-timeout'] === 'none') {
+                req.pipe(res);
+            }
+            if (req.headers['x-timeout'] === 'first') {
+                counter += 1;
+                if (counter === 1) {
+                    const timeoutHandle = setTimeout(() => {
+                        req.pipe(res);
+                    }, 1000);
+                    res.on('close', () => clearTimeout(timeoutHandle));
+                } else {
+                    req.pipe(res);
+                }
+            }
+        });
+        serverHandle1.listen(44441);
+        serverHandle1.on('listening', () => ready());
+    });
+    afterAll(() => {
+        serverHandle1.close();
+    });
+
+    test('timeout #0', (done) => {
+        ezs.use(statements);
+        const input = ['1a', '2a', '3a', '4a', '5a'];
+        const output = [];
+        from(input)
+            .pipe(ezs('URLConnect', {
+                url: 'http://127.0.0.1:44441/',
+                json: true,
+                header: 'x-timeout:none',
+            }))
+            .pipe(ezs.catch())
+            .on('error', done)
+            .on('data', (chunk) => {
+                output.push(chunk);
+            })
+            .on('end', () => {
+                expect(output.length).toBe(5);
+                done();
+            });
+    });
+
+
+    test('timeout #1', (done) => {
+        ezs.use(statements);
+        const input = ['1a', '2a', '3a', '4a', '5a'];
+        from(input)
+            .pipe(ezs('URLConnect', {
+                url: 'http://127.0.0.1:44441/',
+                json: true,
+                retries: 1,
+                timeout: 100,
+                header: 'x-timeout:all',
+            }))
+            .pipe(ezs.catch())
+            .on('error', (e) => {
+                try {
+                    expect(e.message).toEqual(expect.stringContaining('timeout'));
+                    done();
+                } catch(ee) {
+                    done(ee);
+                }
+            })
+            .on('end', () => {
+                done(new Error('Error is the right behavior'));
+            });
+    });
+
+    test('timeout #2', (done) => {
+        ezs.use(statements);
+        const input = ['1a', '2a', '3a', '4a', '5a'];
+        from(input)
+            .pipe(ezs('URLConnect', {
+                url: 'http://127.0.0.1:44441/',
+                json: true,
+                retries: 2,
+                timeout: 100,
+                header: 'x-timeout:all',
+            }))
+            .pipe(ezs.catch())
+            .on('error', (e) => {
+                try {
+                    expect(e.message).toEqual(expect.stringContaining('timeout'));
+                    done();
+                } catch(ee) {
+                    done(ee);
+                }
+            })
+            .on('end', () => {
+                done(new Error('Error is the right behavior'));
+            });
+    });
+
+    test('timeout #4', (done) => {
+        ezs.use(statements);
+        const input = ['1a', '2a', '3a', '4a', '5a'];
+        const output = [];
+        from(input)
+            .pipe(ezs('URLConnect', {
+                url: 'http://127.0.0.1:44441/',
+                json: true,
+                retries: 2,
+                timeout: 900,
+                header: 'x-timeout:first',
+            }))
+            .pipe(ezs.catch())
+            .on('error', done)
+            .on('data', (chunk) => {
+                output.push(chunk);
+            })
+            .on('end', () => {
+                expect(output.length).toBe(5);
+                done();
+            });
+    });
+
+
+    test.skip('timeout #5', (done) => {
+        ezs.use(statements);
+        const input = [
+            {
+                value: ['a', 'b']
+            },
+            {
+                value: ['b', 'c', 'd']
+            },
+            {
+                value: ['d']
+            }
+        ];
+        const output = [];
+        from(input)
+            .pipe(ezs('URLConnect', {
+                url: 'http://127.0.0.1:44441/',
+                json: true,
+                retries: 2,
+                timeout: 900,
+                header: 'x-timeout:first',
+            }))
+            .pipe(ezs.catch())
+            .on('error', done)
+            .on('data', (chunk) => {
+                output.push(chunk);
+            })
+            .on('end', () => {
+                expect(output.length).toBe(5);
+                done();
+            });
+    });
+
+
+
+});
+
