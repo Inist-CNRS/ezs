cd091acd91a6b0042e6c0e1b9cfef87e64126007
Nicolas Thouvenin <nthouvenin@gmail.com>
2023-04-12 14:40:52 +0200

    fix: 🐛 missing files

---
+++ packages/basics/src/txt-inflection.js
@@ -0,0 +1,53 @@
+import get from 'lodash.get';
+import { transform } from 'inflection';
+
+const TXTInflection = (data, feed, ctx) => {
+    if (ctx.isLast()) {
+        return feed.close();
+    }
+    const transformations = [].concat(ctx.getParam('transform', [])).filter(Boolean);
+    const path = ctx.getParam('path', 'value');
+    const value = get(data, path, '');
+
+    const str = Array.isArray(value)
+        ? value.map((item) => (typeof item === 'string' ? item : '')).join(' ')
+        : value;
+    const result = str ? transform( str, transformations) : value;
+
+    feed.write({ ...data, [path]: result });
+    return feed.end();
+};
+
+/**
+ * Take a `String` and inflect it with or more transformers from this list
+ *  pluralize, singularize, camelize, underscore, humanize, capitalize,
+ *  dasherize, titleize, demodulize, tableize, classify, foreign_key, ordinalize
+ *
+ * Input:
+ *
+ * ```json
+ * { "id": 1, "value": "all job" }
+ * ```
+ * Script:
+ * ```ini
+ * [TXTInflection]
+ * transform = pluralize
+ * transform = capitalize
+ * transform = dasherize
+ * ```
+ *
+ * Output:
+ *
+ * ```json
+ * { "id": 1, "value": "All-jobs" }
+ * ```
+ *
+ * @name TXTInflection
+ * @param {String} [path="value"] path of the field to segment
+ * @param {String} [transform] name of a transformer
+ * @returns {String[]}
+ * @see https://www.npmjs.com/package/inflection
+ */
+export default {
+    TXTInflection,
+};
