770e37a3946932fbf3e86a2cefad4ec377c26eeb
Nicolas Thouvenin <nthouvenin@gmail.com>
2023-05-16 17:30:30 +0200

    test: 💍 add standalone tests

--- packages/core/test/fork.js
+++ packages/core/test/fork.js
@@ -44,6 +44,36 @@ describe('fork)', () => {
                 done();
             });
     });
+    it('#1 (standalone)', (done) => {
+        let res = 0;
+        const script = `
+            [assign]
+            path = a
+            value = 99
+        `;
+        from([
+            { a: 1, b: 9 },
+            { a: 2, b: 9 },
+            { a: 1, b: 9 },
+            { a: 1, b: 9 },
+            { a: 1, b: 9 },
+        ])
+            .pipe(ezs('fork', {
+                script,
+                standalone: true,
+            }))
+            .pipe(ezs.catch())
+            .on('error', done)
+            .on('data', (chunk) => {
+                assert(typeof chunk === 'object');
+                res += chunk.a;
+            })
+            .on('end', () => {
+                assert.equal(6, res);
+                done();
+            });
+    });
+
     it('#2', (done) => {
         const script = `
             [boum]
@@ -68,6 +98,33 @@ describe('fork)', () => {
                 done(new Error('Error is the right behavior'));
             });
     });
+
+    it('#2 (standalone)', (done) => {
+        const script = `
+            [boum]
+        `;
+        from([
+            { a: 1, b: 9 },
+            { a: 2, b: 9 },
+            { a: 1, b: 9 },
+            { a: 1, b: 9 },
+            { a: 1, b: 9 },
+        ])
+            .pipe(ezs('fork', {
+                script,
+                standalone: true,
+            }))
+            .pipe(ezs.catch())
+            .on('error', (e) => {
+                expect(e.message).toEqual(expect.stringContaining('boum'));
+                done();
+            })
+            .on('data', () => true)
+            .on('end', () => {
+                done(new Error('Error is the right behavior'));
+            });
+    });
+
     it('#3', (done) => {
         const script = `
             [aie]
@@ -92,6 +149,33 @@ describe('fork)', () => {
                 done(new Error('Error is the right behavior'));
             });
     });
+
+    it('#3 (standalone)', (done) => {
+        const script = `
+            [aie]
+        `;
+        from([
+            { a: 1, b: 9 },
+            { a: 2, b: 9 },
+            { a: 1, b: 9 },
+            { a: 1, b: 9 },
+            { a: 1, b: 9 },
+        ])
+            .pipe(ezs('fork', {
+                script,
+                standalone: true,
+            }))
+            .pipe(ezs.catch())
+            .on('error', (e) => {
+                expect(e.message).toEqual(expect.stringContaining('aie!'));
+                done();
+            })
+            .on('data', () => true)
+            .on('end', () => {
+                done(new Error('Error is the right behavior'));
+            });
+    });
+
     it('#4', (done) => {
         const script = `
             [fake]
@@ -116,4 +200,31 @@ describe('fork)', () => {
                 done(new Error('Error is the right behavior'));
             });
     });
+
+    it('#4 (standalone)', (done) => {
+        const script = `
+            [fake]
+        `;
+        from([
+            { a: 1, b: 9 },
+            { a: 2, b: 9 },
+            { a: 1, b: 9 },
+            { a: 1, b: 9 },
+            { a: 1, b: 9 },
+        ])
+            .pipe(ezs('fork', {
+                script,
+                standalone: true,
+            }))
+            .pipe(ezs.catch())
+            .on('error', (e) => {
+                expect(e.message).toEqual(expect.stringContaining('fake'));
+                done();
+            })
+            .on('data', () => true)
+            .on('end', () => {
+                done(new Error('Error is the right behavior'));
+            });
+    });
+
 });
