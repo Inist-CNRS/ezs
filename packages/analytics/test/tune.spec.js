7deb2a015abe7a74878733689b123de7a969e77a
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2023-12-14 09:20:19 +0100

    test (analytics): add different data set to tune

--- packages/analytics/test/tune.spec.js
+++ packages/analytics/test/tune.spec.js
@@ -5,38 +5,125 @@ import tune from '../src/tune';
 
 ezs.addPath(__dirname);
 
-const simpleObject =  [
+const simpleData =  [
     {
-        id: 1,
-        value: 1
+        'id': 1,
+        'value': 1
     },
     {
-        id: 2,
-        value: 2
+        'id': 2,
+        'value': 2
+    }
+];
+
+const arrayData =  [
+    {
+        'id': 1,
+        'value': [1, 1]
+    },
+    {
+        'id': 2,
+        'value': [2, 2]
     }
 ];
 
 describe('tune', () => {
-    it('should normalize simple object', async () => {
-        ezs.use({ tune });
 
-        const res = await (new Promise((resolve) => {
-            const result = [];
-            from(simpleObject)
-                .pipe(ezs('tune'))
-                .on('data', (chunk) => {
-                    assert(typeof chunk === 'object');
-                    result.push(chunk);
-                })
-                .on('end', () => {
-                    resolve(result);
-                });
-        }));
+    describe('normalize, simple object', () => {
 
-        assert.equal(res[0].id, '0000000000000000001.00000000000000000000');
-        assert.equal(res[1].id, '0000000000000000002.00000000000000000000');
+        it('should normalize (path = id)', async () => {
+            ezs.use({ tune });
 
-        assert.equal(res[0].value, simpleObject[0]);
-        assert.equal(res[1].value, simpleObject[1]);
+            const res = await (new Promise((resolve) => {
+                const result = [];
+                from(simpleData)
+                    .pipe(ezs('tune'))
+                    .on('data', (chunk) => {
+                        assert(typeof chunk === 'object');
+                        result.push(chunk);
+                    })
+                    .on('end', () => {
+                        resolve(result);
+                    });
+            }));
+
+            assert.equal(res[0].id, '0000000000000000001.00000000000000000000');
+            assert.equal(res[1].id, '0000000000000000002.00000000000000000000');
+
+            assert.equal(res[0].value, simpleData[0]);
+            assert.equal(res[1].value, simpleData[1]);
+        });
+
+        it('should normalize (path = value)', async () => {
+            ezs.use({ tune });
+
+            const res = await (new Promise((resolve) => {
+                const result = [];
+                from(simpleData)
+                    .pipe(ezs('tune', { path: 'value' }))
+                    .on('data', (chunk) => {
+                        assert(typeof chunk === 'object');
+                        result.push(chunk);
+                    })
+                    .on('end', () => {
+                        resolve(result);
+                    });
+            }));
+
+            assert.equal(res[0].id, '0000000000000000001.00000000000000000000');
+            assert.equal(res[1].id, '0000000000000000002.00000000000000000000');
+
+            assert.equal(res[0].value, simpleData[0]);
+            assert.equal(res[1].value, simpleData[1]);
+        });
+    });
+
+    describe('normalize, array object', () => {
+
+        it('should normalize (path = id)', async () => {
+            ezs.use({ tune });
+
+            const res = await (new Promise((resolve) => {
+                const result = [];
+                from(arrayData)
+                    .pipe(ezs('tune'))
+                    .on('data', (chunk) => {
+                        assert(typeof chunk === 'object');
+                        result.push(chunk);
+                    })
+                    .on('end', () => {
+                        resolve(result);
+                    });
+            }));
+
+            assert.equal(res[0].id, '0000000000000000001.00000000000000000000');
+            assert.equal(res[1].id, '0000000000000000002.00000000000000000000');
+
+            assert.equal(res[0].value, arrayData[0]);
+            assert.equal(res[1].value, arrayData[1]);
+        });
+
+        it('should normalize (path = value)', async () => {
+            ezs.use({ tune });
+
+            const res = await (new Promise((resolve) => {
+                const result = [];
+                from(arrayData)
+                    .pipe(ezs('tune', { path: 'value' }))
+                    .on('data', (chunk) => {
+                        assert(typeof chunk === 'object');
+                        result.push(chunk);
+                    })
+                    .on('end', () => {
+                        resolve(result);
+                    });
+            }));
+
+            assert.equal(res[0].id, '1.1');
+            assert.equal(res[1].id, '2.2');
+
+            assert.equal(res[0].value, arrayData[0]);
+            assert.equal(res[1].value, arrayData[1]);
+        });
     });
 });
