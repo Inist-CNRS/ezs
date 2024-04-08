ba8fcaf5924dbff32fd2d26024fb2cff67d836a2
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2024-04-08 13:06:05 +0200

    test (libpostal): add and update test

--- packages/libpostal/test/expand.spec.js
+++ packages/libpostal/test/expand.spec.js
@@ -5,19 +5,29 @@ import runEzs from '../test-utils/runEzs';
 
 ezs.addPath(__dirname);
 
-describe('expandAddress', () => {
+describe('expandAddress, expandAddressWith', () => {
 
     beforeAll(() => {
         ezs.use({ expandAddress, expandAddressWith });
     });
 
-    describe('expandAddress, simple object', () => {
+    describe('expandAddress', () => {
         const simpleData =  [
             'Barboncino 781 Franklin Ave, Crown Heights, Brooklyn, NY 11238'
         ];
 
-        it('should expandAddress', async () => {
+        const simpleData2 =  [
+            ['Barboncino 781 Franklin Ave, Crown Heights, Brooklyn, NY 11238']
+        ];
+
+        it.skip('should expandAddress with simple array of string', async () => {
             const result = await runEzs(ezs, simpleData, 'expandAddress');
+
+            expect(result[0]).not.toBeNull();
+        });
+
+        it('should expandAddress with simple array of array of string', async () => {
+            const result = await runEzs(ezs, simpleData2, 'expandAddress');
 
             expect(result[0]).not.toBeNull();
 
@@ -25,7 +35,7 @@ describe('expandAddress', () => {
         });
     });
 
-    describe('expandAddressWith, simple object', () => {
+    describe('expandAddressWith', () => {
         const simpleData =  [
             {value: 'Barboncino 781 Franklin Ave, Crown Heights, Brooklyn, NY 11238'}
         ];
@@ -35,7 +45,9 @@ describe('expandAddress', () => {
 
             expect(result[0]).not.toBeNull();
 
-            console.log(result);
+            expect(result[0].id).toEqual(simpleData[0].value);
+
+            console.log(result[0].value);
         });
     });
 });
