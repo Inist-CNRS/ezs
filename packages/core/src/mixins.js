0a55722846605f5e07fbcb5ec9eec0dc9250b9ef
Nicolas Thouvenin <nthouvenin@gmail.com>
2025-01-17 15:45:37 +0100

    feat: 🎸 drop useless feature

--- packages/core/src/mixins.js
+++ packages/core/src/mixins.js
@@ -1,5 +1,3 @@
-import { compileExpression } from 'filtrex';
-
 const fix = (input, ...args) => {
     if (args.length === 1) {
         return args[0];
@@ -11,13 +9,10 @@ const prepend = (input, prefix = '') => String(prefix).concat(input);
 
 const append = (input, suffix = '') => String(input).concat(suffix);
 
-const compute = (input, expr) => compileExpression(expr)(input);
-
 const self = (input) => input;
 
 export default {
     fix,
-    compute,
     self,
     prepend,
     append,
