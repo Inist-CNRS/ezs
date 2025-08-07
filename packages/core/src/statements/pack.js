80e60d95c0b1d7164c863944cd781664ce54b014
Nicolas Thouvenin <nthouvenin@gmail.com>
2025-08-07 14:03:10 +0200

    feat: add "path=" to [pack] and [unpack]

--- packages/core/src/statements/pack.js
+++ packages/core/src/statements/pack.js
@@ -1,13 +1,18 @@
+import set  from 'lodash/set.js';
+
 const eol = '\n';
 /**
  * Take all `Object`, throw encoded `String`
  *
  * @name pack
+ * @param {String} [path] path to set in the serialize object (default: none)
  * @returns {String}
  */
 export default function pack(data, feed) {
     if (this.isLast()) {
         return feed.close();
     }
-    return feed.send(JSON.stringify(data).concat(eol));
+    const path = [].concat(this.getParam('path', [])).filter(Boolean).shift();
+    const value = !path ? data : set({}, path, data);
+    return feed.send(JSON.stringify(value).concat(eol));
 }
